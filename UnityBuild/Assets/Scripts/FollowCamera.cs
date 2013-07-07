0c150c8f02960cb36e88a3a015c13658de154235
Rogelio E. Cardona-Rivera <mail@rogel.io>
2013-07-07 10:53:30 -0400

    Changed the follow camera to be very simple.

--- UnityBuild/Assets/Scripts/FollowCamera.cs
+++ UnityBuild/Assets/Scripts/FollowCamera.cs
@@ -4,19 +4,13 @@ using System.Collections;
 public class FollowCamera : MonoBehaviour {
 	
 	public GameObject target;
-	public float damping = 1;
-	
-	private Vector3 offset;
-	
-	void Start () {
-		offset = target.transform.position - this.transform.position;
-	}
+	public float zOffset = 10.0f;
+	public float yOffset = 1.2f;
 	
 	void LateUpdate () {
-		float desiredAngle = target.transform.eulerAngles.y;
-		//Quaternion rotation = Quaternion.Euler(0.0f, desiredAngle, 0.0f);
-		gameObject.transform.position = new Vector3(target.transform.position.x, target.transform.position.y+5, target.transform.position.z+10);
-		
-		gameObject.transform.LookAt(target.transform);
+		float newZ = target.transform.position.z + zOffset;
+		float newY = target.transform.position.y + yOffset;
+		float newX = target.transform.position.x;
+		transform.position = new Vector3(newX, newY, newZ);
 	}
 }
