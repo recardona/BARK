538c4e287c2b11f288d6138109252ff449d70a46
Ian Coleman <colemip@gmail.com>
2013-07-07 11:58:58 -0400

    Displaying message when entering level end trigger

--- UnityBuild/Assets/Scripts/EndLevelTriggerScript.cs
+++ UnityBuild/Assets/Scripts/EndLevelTriggerScript.cs
@@ -2,7 +2,22 @@ using UnityEngine;
 using System.Collections;
 
 public class EndLevelTriggerScript : MonoBehaviour {
+	private bool isLevelComplete = false;
+	
 	void OnTriggerEnter(Collider other) {
-		Debug.Log("LEVEL COMPLETED");
+		isLevelComplete = true;
+		//Debug.Log("LEVEL COMPLETED");
+//		GUIText endLevelText = new GUIText();
+//		endLevelText.text = "Level Completed!!";
+//		endLevelText.alignment = TextAlignment.Center;
+//		endLevelText.enabled = true;
+		
+		//Debug.Log(endLevelText);
     }
+	
+	void OnGUI () {
+		if(isLevelComplete) {
+			GUI.Label (new Rect (100, 300, 100, 50), "LEVEL COMPLETED!!");
+		}
+	}
 }
