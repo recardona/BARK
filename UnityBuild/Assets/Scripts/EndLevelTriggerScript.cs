a59d30e47e64398915c500b5042f8db1abe348f2
Ian Coleman <colemip@gmail.com>
2013-07-07 18:34:51 -0400

    Display prompt to restart at game completed, restart gaming after pressing prompt key

--- UnityBuild/Assets/Scripts/EndLevelTriggerScript.cs
+++ UnityBuild/Assets/Scripts/EndLevelTriggerScript.cs
@@ -2,8 +2,16 @@ using UnityEngine;
 using System.Collections;
 
 public class EndLevelTriggerScript : MonoBehaviour {
-	public string levelCompleteMessage = "LEVEL COMPLETED!!";
+	public string levelCompleteMessage = "LEVEL COMPLETED!! Press E to restart";
 	private bool isLevelComplete = false;
+	
+	void Update() {
+		if(isLevelComplete) {
+			if(Input.GetKey("e")) {
+					Application.LoadLevel(0);
+			}
+		}	
+	}
 	
 	void OnTriggerEnter(Collider other) {
 		isLevelComplete = true;
@@ -11,7 +19,10 @@ public class EndLevelTriggerScript : MonoBehaviour {
 	
 	void OnGUI () {
 		if(isLevelComplete) {
-			GUI.Label (new Rect (100, 300, 100, 50), levelCompleteMessage);
+			GUI.Label (new Rect (100, 300, 100, 50), levelCompleteMessage);			
+			if(Input.GetKey("e")) {
+				Application.LoadLevel(0);
+			}
 		}
 	}
 }
